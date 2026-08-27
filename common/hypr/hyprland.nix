1fc04ddc6c3fa8f59a91ddb0d995f4a311abbb08
Neal van Veen <nealvanveen@gmail.com>
2026-08-27 16:05:51 +0200

    change cursor

--- common/hypr/hyprland.nix
+++ common/hypr/hyprland.nix
@@ -1,3 +1,12 @@
+{ pkgs, ... }:
 {
   xdg.configFile."hypr/hyprland.lua".source = ./hyprland.lua;
+
+  home.pointerCursor = {
+    enable = true;
+    package = pkgs.bibata-cursors;
+    name = "Bibata-Modern-Classic";
+    size = 24;
+    gtk.enable = true;
+  };
 }
