a0e891efbfb5b9a06b0680a492b0fb155bddd7db
Neal van Veen <nealvanveen@gmail.com>
2026-09-16 00:16:54 +0200

    wip

--- hosts/nealdesk/default.nix
+++ hosts/nealdesk/default.nix
@@ -30,6 +30,28 @@
 
   boot.initrd.systemd.enable = true;
 
+  boot.initrd.availableKernelModules = [ "r8169" ];
+
+  boot.initrd.network.enable = true;
+  boot.initrd.systemd.network = {
+    enable = true;
+    networks."10-lan" = {
+      matchConfig.Name = "en*";
+      networkConfig.DHCP = "yes";
+      dhcpV4Config.ClientIdentifier = "mac";
+    };
+  };
+
+  boot.initrd.network.ssh = {
+    enable = true;
+    port = 2222;
+    hostKeys = [ "/etc/secrets/initrd/ssh_host_ed25519_key" ];
+    authorizedKeys = [
+      ''command="systemctl default" ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDFuFQCFGyUL3NY2xdTU6V7TQ5GN1IGL8UTxmS0X9Sq0 neal@nealmsi''
+      ''restrict,pty,command="systemctl default" ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHTXW4wVy7AIK2FA36K+OuKKmv5mMf/ef90gJqyE0DHk phone-nealdesk-unlock''
+    ];
+  };
+
   boot.loader.systemd-boot.enable = lib.mkForce false; # initial pre-enroll ba7l3qroot step
   boot.loader.efi.canTouchEfiVariables = true;
 
